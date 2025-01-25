2d6579b8371d7dace7c4bd95dedc66f5edb7954b
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2025-01-25 17:32:18 +0100

    Update Sleuth.swift
    - using a single call to xivapi to get the item names
    - moved a few things around

--- SaleSleuth/Sleuth.swift
+++ SaleSleuth/Sleuth.swift
@@ -32,54 +32,39 @@ struct Sleuth{
             exit(0)
         }
         
-        let names: [String] = input.split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
-        
-        var itemNames = [Int : String]() //storing itemNames here to limit xivapi calls
-        var marketData = [CurrentlyShownView]()
-        var results = [String]()
-        var runningTotal = 0
-        
         print("fetching ItemIDs of marketable items..".hex(textColor))
         let marketableItems = await universalis.getMarketableItems().result
         let chunks = marketableItems?.chunked(into: 100)
         print("total marketable items: \(marketableItems?.count ?? 0)".green)
         
         print("fetching \(world) market data".hex(textColor))
+        var marketData = [CurrentlyShownView]()
+
         for chunk in Progress(chunks ?? [], configuration: [ProgressBarLine(), ProgressPercent(decimalPlaces: 2), ProgressTimeEstimates()]) {
-            var currentData = await universalis.getCurrentData(worldDcRegion: world, itemIds: chunk, queryItems: nil).result
+            let currentData = await universalis.getCurrentData(worldDcRegion: world, itemIds: chunk)
             
-            while currentData == nil {
-                try? await Task.sleep(for: .milliseconds(250))
-                //this shouldn't happen
-                currentData = await universalis.getCurrentData(worldDcRegion: world, itemIds: chunk, queryItems: nil).result
-            }
-            
-            currentData?.items?.values.forEach({ currentlyShownView in
+            currentData.result?.items?.values.forEach({ currentlyShownView in
                 marketData.append(currentlyShownView)
             })
         }
         
-        let totalListings = marketData.reduce(0) { $0 + ($1.listings?.count ?? 0) }.formatted()
-        print("processing \(world) market data (\(totalListings) listings)".hex(textColor))
-        for data in Progress(marketData, configuration: [ProgressBarLine(), ProgressPercent(decimalPlaces: 2), ProgressTimeEstimates()]) {
+        let retainerNames: [String] = input.split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
+        let matchingData = marketData.filter { $0.listings?.filter({ retainerNames.contains($0.retainerName ?? "") }).count ?? 0 > 0 }
+        let matchingItemIds = Set(matchingData.map { $0.itemID }).map { $0 }
+        let items: [ItemMinimal]? = await xivapi.getSheetRows(.Item, rows: matchingItemIds, queryItems: [URLQueryItem(name: "fields", value: "Name,Icon,Description")])
+        
+        var results = [String]()
+        var runningTotal = 0
+        var itemNames = [Int : String]()
+        items?.forEach({ itemNames[$0.row_id] = $0.name })
+                
+        for data in Progress(matchingData, configuration: [ProgressBarLine(), ProgressPercent(decimalPlaces: 2), ProgressTimeEstimates()]) {
             guard let listings = data.listings else { continue }
-            
-            let itemID = data.itemID
-            
-            for listing in listings {
-                guard let retainerName = listing.retainerName else { continue }
-                if names.contains(retainerName){
-                    
-                    if !itemNames.keys.contains(itemID) {
-                        let itemName = await xivapi.getItemMinimal(itemID)?.name
-                        itemNames[itemID] = itemName != nil ? itemName : itemID.description
-                    }
-                    
-                    let result = "\(retainerName) - \(itemNames[itemID] ?? itemID.description) x\(listing.quantity) @ \(listing.pricePerUnit.formatted()) gil/each"
-                    
-                    results.append(result)
-                    runningTotal += listing.total
-                }
+            let matches = listings.filter ({ retainerNames.contains($0.retainerName ?? "") })
+            matches.forEach { listing in
+                runningTotal += listing.total
+                let result = "\(listing.retainerName ?? "[retainer name]") - \(itemNames[data.itemID] ?? data.itemID.description) x\(listing.quantity) @ \(listing.pricePerUnit.formatted()) gil/each"
+                results.append(result)
             }
         }
         
